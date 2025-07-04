3a6866dd90636e293cd4119751f9a73b548fd45a
yuewang <yuewang@academic.rrc.ca>
2025-07-04 15:53:02 -0500

    1. Migrate your database.  2.Add validation to the Product model to ensure that the product always includes a title, price and stock_quantity. Ensure that the validations are working by trying to add an invalid product from your rails console or seed script.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
