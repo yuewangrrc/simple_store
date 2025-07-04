f74b90e2d5ae14b150d90b3b38a87b4e6e06740b
yuewang <yuewang@academic.rrc.ca>
2025-07-04 16:22:59 -0500

    Updated product index and show views such that all products are display with the name of their associated category.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category).all
   end
 
   def show
-    @product = Product.find(params[:id])
+    @product = Product.includes(:category).find(params[:id])
   end
 end
