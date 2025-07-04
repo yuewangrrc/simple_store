61b14864db7ed78d6cc3d9d12953b331636c3c9e
yuewang <yuewang@academic.rrc.ca>
2025-07-04 16:16:18 -0500

    Added the associations (belongs_to / has_many) to the Category and Product models.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :products, dependent: :destroy
+  
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :category
+  
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
